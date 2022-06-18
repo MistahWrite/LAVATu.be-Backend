f2e9f9e96c8d2dee398b564cb3b11529814c0513
Chad Jay Harrington <MistahWrite@gmail.com>
2022-06-17 20:59:32 -0600

    First commit

---
+++ util/location.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+async function getCoordsForAddress(address) {
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
+
+  if (data.status === 'OK') {
+    console.log("Formatted Address: " + data.results[0].formatted_address);
+    const coordinates = data.results[0].geometry.location;
+    return coordinates;
+  } else {
+    const error = new HttpError(
+      'An error occurred with the Google Maps API.  ' + data.status + "\n" + API_KEY ,
+      404
+    );
+    throw error;
+  }
+
+  
+}
+
+module.exports = getCoordsForAddress;
