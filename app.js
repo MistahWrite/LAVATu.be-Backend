032f2d682fa21367f73841139238cca583fe26a5
Chad Jay Harrington <MistahWrite@gmail.com>
2022-06-23 15:48:58 -0600

    Trying to fix issue

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public', 'build')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,8 +29,6 @@ app.use((req, res, next) => {
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
-
-app.use(express.static(path.join('public', 'build')));
 
 app.use((req, res, next) => {
   res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
